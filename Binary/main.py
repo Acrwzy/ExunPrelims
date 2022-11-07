d3cf287fe1a2c59608d487a89ce941eb9c38d260
Aarav Kaushik <67853273+Acrwzy@users.noreply.github.com>
2022-11-07 23:48:35 +0530

    Begin: keras

--- Binary/main.py
+++ Binary/main.py
@@ -14,15 +14,27 @@ from tensorflow.keras.preprocessing import image
 train = ImageDataGenerator(rescale=1/255)
 test = ImageDataGenerator(rescale=1/255)
 
-# located the Train and Test folders; set size as deisered 
+# Located the Train and Test folders; set size as deisered 
 
-train_dataset = train.flow_from_directory("train/", 
-                                          target_size=(28,28), 
+train_dataset = train.flow_from_directory("Train/", 
+                                          target_size=(150,150), 
                                           batch_size = 32,
                                           class_mode = 'binary')
                                          
 test_dataset = test.flow_from_directory("Test/",
-                                        target_size=(28,28),
+                                        target_size=(150,150),
                                         batch_size =32,
                                         class_mode = 'binary')
 
+# Begin: keras, The Sequential Funtion!
+
+model = keras.Sequential()
+
+# Layer created with 32 filters, 5x5 kernel size, 150x150 resolution and 2 dimensions
+
+model.add(keras.layers.Conv2D(32, (5,5), activation='relu', input_shape=(150,150,2)))
+model.add(keras.layers.MaxPool2D(2,2))
+
+# Output layer with single neuron which gives 0 for Cat or 1 for Dog 
+#Here we use sigmoid activation function which makes our model output to lie between 0 and 1
+model.add(keras.layers.Dense(1,activation='sigmoid'))
