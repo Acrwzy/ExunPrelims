f4969c14a5193b28d637afb6e5ce3d5b3dfa5cae
Aarav Kaushik <67853273+Acrwzy@users.noreply.github.com>
2022-11-08 00:01:44 +0530

    Finished, I believe

--- Binary/main.py
+++ Binary/main.py
@@ -11,22 +11,22 @@ from tensorflow.keras.preprocessing import image
 
 # Train and Test folders defined
 
-train = ImageDataGenerator(rescale=1/255)
-test = ImageDataGenerator(rescale=1/255)
+train = ImageDataGenerator(rescale = 1/255)
+test = ImageDataGenerator(rescale = 1/255)
 
 # Located the Train and Test folders; set size as deisered 
 
-train_dataset = train.flow_from_directory("Train/", 
+train_dataset = train.flow_from_directory("/Train/", 
                                           target_size=(150,150), 
                                           batch_size = 32,
                                           class_mode = 'binary')
                                          
-test_dataset = test.flow_from_directory("Test/",
+test_dataset = test.flow_from_directory("/Test/",
                                         target_size=(150,150),
-                                        batch_size =32,
+                                        batch_size = 32,
                                         class_mode = 'binary')
 
-# Begin: keras, The Sequential Funtion!
+# Begin: keras
 
 model = keras.Sequential()
 
@@ -35,6 +35,19 @@ model = keras.Sequential()
 model.add(keras.layers.Conv2D(32, (5,5), activation='relu', input_shape=(150,150,2)))
 model.add(keras.layers.MaxPool2D(2,2))
 
-# Output layer with single neuron which gives 0 for Cat or 1 for Dog 
-#Here we use sigmoid activation function which makes our model output to lie between 0 and 1
+# Sigmod is used here to diffrenciate between 0's and 1's
+
 model.add(keras.layers.Dense(1,activation='sigmoid'))
+
+# Compiling the AI
+
+model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+# Training the model: 
+
+model.fit_generator(train_dataset,
+                    steps_per_epoch = 0.5,
+                    epochs = 16,
+                    validation_data = test_dataset)
+
+                        
